5fe084de4ac2697b02947e915779f7d5580ffed2
August Strickland <august@elijiahstrickland.dev>
2024-03-14 23:52:02 -0700

    Fix Icons on powermenu

--- polybar/colorblocks/scripts/powermenu.sh
+++ polybar/colorblocks/scripts/powermenu.sh
@@ -5,17 +5,17 @@
 ## Github  : @adi1090x
 ## Twitter : @adi1090x
 
-dir="~/.config/polybar/colorblocks/scripts/rofi"
+dir="$HOME/.config/polybar/colorblocks/scripts/rofi"
 uptime=$(uptime -p | sed -e 's/up //g')
 
 rofi_command="rofi -no-config -theme $dir/powermenu.rasi"
 
 # Options
-shutdown=" Shutdown"
-reboot=" Restart"
-lock=" Lock"
-suspend=" Sleep"
-logout=" Logout"
+shutdown="⏻ Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend="󰤄 Sleep"
+logout="󰍃 Logout"
 
 # Confirmation
 confirm_exit() {
