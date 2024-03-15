dc4f32c751a05863db860f1e562ce805565271c3
August Strickland <august@elijiahstrickland.dev>
2024-03-14 23:18:45 -0700

    Adding shit again

--- install.sh
+++ install.sh
@@ -67,7 +67,7 @@ select wm in i3 Hyprland Quit; do
         "i3")
             if [[ $distro == *"arch"* ]]; then
                 sudo pacman -Syq --noconfirm --needed yay
-                yay -Syq --noconfirm --needed i3-gaps-rounded-git i3lock-fancy i3status i3blocks polybar dunst brave-browser
+                yay -Syq --noconfirm --needed i3-gaps-rounded-git i3lock-fancy i3status i3blocks polybar dunst brave-browser pywal-git nitrogen
             fi
             printf "Copying i3 config...\n"
             check_for_config i3
@@ -87,7 +87,7 @@ select wm in i3 Hyprland Quit; do
             break;;
         "hyprland")
             if [[ $distro == *"arch"* ]]; then
-                sudo pacman -Syq --noconfirm --needed hyprland waybar brave-browser
+                sudo pacman -Syq --noconfirm --needed hyprland waybar brave-browser pywal-git nitrogen
             fi
             printf "Copying Hyprland config...\n"
             check_for_config hypr
@@ -200,10 +200,15 @@ sudo cp -r "$HOME/repos/.dotfiles/nvim" /root/.config/ # Copy config for root
 
 printf "\nDone!\n\n"
 
-# Move fonts
+# Copy Nerd Fonts
 printf "\nInstalling Nerd Fonts...\n\n"
-sudo cp -r misc/fonts/* /usr/share/fonts
+sudo cp -r "$HOME/repos/.dotfiles/misc/fonts/*" /usr/share/fonts
 fc-cache -fv # Refresh font cache
+
+# Copy Wallpapers
+printf "\nInstalling Wallpapers...\n\n"
+mkdir -p "$HOME/Pictures/wallpapers"
+cp -r "$HOME/repos/.dotfiles/misc/wallpapers/*" "$HOME/Pictures/wallpapers/"
 
 printf "\nDone!\n\n"
 printf "All configs installed!! Exiting...\n"
